c69e09fab74986f71d66951b5f3e3228a46e3118
Gal <galhayne.gmail.com>
2023-03-22 01:39:03 +0200

    add linkdin link

--- src/cmps/Footer.js
+++ src/cmps/Footer.js
@@ -5,8 +5,9 @@ function Footer() {
 
             <footer className="bg-light text-center text-lg-start">
                 <div className="text-center p-3" >
-                    © 2023 Copyright:
-                    <a className="text-dark" href="https://www.linkedin.com/in/gal-hayne-0a1076267/">GalHayne.com</a>
+                    © 2023 Copyright: Gal Hayne
+                    <a className="text-dark" href="https://www.linkedin.com/in/%E2%80%AAgal-hayne%E2%80%AC%E2%80%8F-0a1076267/" target="_blank" rel="noopener noreferrer"><i class="bi bi-linkedin"></i></a>
+
                 </div>
             </footer>
         </div>
