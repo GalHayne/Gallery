9d7a942e48eaf99c1a8e1ade91a559b1539cec9e
Gal <galhayne.gmail.com>
2023-03-22 17:25:28 +0200

    change let name

--- src/cmps/GalleryDisplayType.js
+++ src/cmps/GalleryDisplayType.js
@@ -1,9 +1,9 @@
 function GalleryDisplayType({ onClick = () => { 'default printing no pass to this cmp function' }, circleGallery = true }) {
-    let galleryType = !circleGallery ? "bi bi-box-arrow-right" : "bi bi-border";
+    let galleryDisplayType = !circleGallery ? "bi bi-box-arrow-right" : "bi bi-border";
 
     return (
         <div>
-            <button className="btn btn-secondary m-2" title="Change the way gallery images are displayed" onClick={onClick}><i className={galleryType}></i></button>
+            <button className="btn btn-secondary m-2" title="Change the way gallery images are displayed" onClick={onClick}><i className={galleryDisplayType}></i></button>
         </div>
     )
 
